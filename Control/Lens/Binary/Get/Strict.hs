c7a5b2230f0d5481d1973c16ad9b50cb2a0d024a
startling <tdixon51793@gmail.com>
2013-01-02 08:17:43 -0500

    similarly, got rid of the StateT in Get.

--- Control/Lens/Binary/Get/Strict.hs
+++ Control/Lens/Binary/Get/Strict.hs
@@ -4,8 +4,6 @@ import Control.Applicative
 -- bytestring
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
--- mtl
-import Control.Monad.State
 -- lens
 import Control.Lens
 -- data-default
@@ -14,20 +12,18 @@ import Data.Default
 import Control.Lens.Binary.Serialize
 
 -- | Parse a bytestring into some kind of 'Serialize'.
-newtype Get r = Get (StateT (ByteString, r) Maybe ())
+newtype Get r = Get (ByteString -> r -> Maybe (ByteString, r))
 
 instance Serialize Get where
-  bytes i = Get $ get >>= \(bs, r) ->
-    if B.length bs < i then empty else
-      put (B.drop i bs, B.take i bs)
-  Get b +> l = Get $ get >>= \(bs, cur) ->
-    lift (execStateT b (bs, view l cur)) >>= \(bs, new) ->
-      put (bs, set l new cur)
-  Get a |+| Get b = Get $ a >> b
+  bytes i = Get $ \bs _ -> if B.length bs < i then Nothing else
+    Just (B.drop i bs, B.take i bs)
+  Get b +> l = Get $ \bs r -> b bs (view l r) >>=
+    \(bs, new) -> return (bs, set l new r)
+  Get a |+| Get b = Get $ \bs r -> a bs r >>= uncurry b
 
 -- | Deserialize a value, given the value to start with.
 deserialize' :: Get b -> b -> ByteString -> Maybe b
-deserialize' (Get a) x bs = snd <$> execStateT a (bs, x)
+deserialize' (Get a) x bs = snd <$> a bs x
 
 -- | Deserialize a value, starting with the default value of its type.
 deserialize :: Default b => Get b -> ByteString -> Maybe b
