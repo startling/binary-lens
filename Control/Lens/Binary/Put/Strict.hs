d25846cc566203d3d7f5852cec4a1f0a02f22b82
startling <tdixon51793@gmail.com>
2013-01-02 07:53:10 -0500

    Got rid of that crazy monad stack, replaced it with r -> ByteString

--- Control/Lens/Binary/Put/Strict.hs
+++ Control/Lens/Binary/Put/Strict.hs
@@ -1,7 +1,4 @@
 module Control.Lens.Binary.Put.Strict where
--- mtl
-import Control.Monad.Writer
-import Control.Monad.Reader
 -- bytestring
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
@@ -11,13 +8,13 @@ import Control.Lens
 import Control.Lens.Binary.Serialize
 
 -- | Build a bytestring from some 'Serialize'.
-newtype Put r = Put (ReaderT r (Writer ByteString) ())
+newtype Put r = Put (r -> ByteString)
 
 instance Serialize Put where
-  bytes i = Put $ ask >>= tell . B.take i
-  Put b +> l= Put $ withReaderT (view l) b
-  Put a |+| Put b = Put $ a >> b
+  bytes i = Put $ B.take i
+  Put b +> l= Put $ b . view l
+  Put a |+| Put b = Put $ \bs -> a bs `B.append` b bs
 
 -- | Serialize some value, given a Put action.
 serialize :: Put a -> a -> ByteString
-serialize (Put a) = execWriter . runReaderT a
+serialize (Put a) = a
