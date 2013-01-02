305e574fe1fceb219ee250aaaf5ea4620ffad87a
startling <tdixon51793@gmail.com>
2013-01-01 21:18:33 -0500

    Main module that imports things.

---
+++ Control/Lens/Binary.hs
@@ -0,0 +1,9 @@
+module Control.Lens.Binary
+  ( module Control.Lens.Binary.Serialize
+  , module Control.Lens.Binary.Get
+  , module Control.Lens.Binary.Put )
+  where
+-- binary-lens
+import Control.Lens.Binary.Serialize
+import Control.Lens.Binary.Get
+import Control.Lens.Binary.Put
