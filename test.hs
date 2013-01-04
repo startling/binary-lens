84ee970765f8c44967585247ab671acfe2c4a75d
startling <tdixon51793@gmail.com>
2013-01-04 18:07:23 -0500

    separated out the different tests, so I can run them individually.
    running them all takes some time.

--- test.hs
+++ test.hs
@@ -33,49 +33,71 @@ shouldParseTo :: (?action :: Get b, Eq b, Show b, Default b)
   => ByteString -> b -> Expectation
 a `shouldParseTo` b = deserialize ?action a `shouldBe` Just b
 
+testByte :: Spec
+testByte = describe "byte" $ let ?action = byte in do
+  it "parses single bytes" $ do
+    B.singleton 255 `shouldParseTo` 255
+    B.singleton 123 `shouldParseTo` 123
+  testPutGet byte 1
+
+testWord16le :: Spec
+testWord16le = describe "word16le" $ let ?action = word16le in do
+  it "parses pairs of bytes" $ do
+    B.pack [0xff, 0xff] `shouldParseTo` 0xffff
+  it "deals with endianity correctly" $ do
+    B.pack [0xe8, 0x03] `shouldParseTo` 0x03e8
+  testPutGet word16le 2
+
+testWord16be :: Spec
+testWord16be = describe "word16be" $ let ?action = word16be in do
+  it "parses pairs of bytes" $ do
+    B.pack [0xff, 0xff] `shouldParseTo` 0xffff
+  it "deals with endianity correctly" $ do
+    B.pack [0x03, 0xe8] `shouldParseTo` 0x03e8
+  testPutGet word16be 2
+
+testWord32le :: Spec
+testWord32le = describe "word32le" $ let ?action = word32le in do
+  testPutGet word32le 4
+  it "parses four bytes" $ do
+    B.pack [0xff, 0xff, 0xff, 0xff] `shouldParseTo` 0xffffffff
+  it "deals with endianity correctly" $ do
+    B.pack [0x03, 0xe8, 0x03, 0xe8] `shouldParseTo` 0xe803e803
+
+testWord32be :: Spec
+testWord32be = describe "word32be" $ let ?action = word32be in do
+  testPutGet word32be 4
+  it "parses four bytes" $ do
+    B.pack [0xff, 0xff, 0xff, 0xff] `shouldParseTo` 0xffffffff
+  it "deals with endianity correctly" $ do
+    B.pack [0xe8, 0x03, 0xe8, 0x03] `shouldParseTo` 0xe803e803
+
+testWord64le :: Spec
+testWord64le = describe "word64le" $ let ?action = word64le in do
+  it "parses eight bytes" $ do
+    B.pack [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+      `shouldParseTo` 0xffffffffffffffff
+  it "deals with endianity correctly" $ do
+    B.pack [0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]
+      `shouldParseTo` 0xffeeddccbbaa9988
+  testPutGet word64le 8
+
+testWord64be :: Spec
+testWord64be = describe "word64be" $ let ?action = word64be in do
+  it "parses eight bytes" $ do
+    B.pack [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+      `shouldParseTo` 0xffffffffffffffff
+  it "deals with endianity correctly" $ do
+    B.pack [0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]
+      `shouldParseTo` 0x8899aabbccddeeff
+  testPutGet word64be 8
+
+main :: IO ()
 main = hspec $ do
-  describe "byte" $ let ?action = byte in do
-    it "parses single bytes" $ do
-      B.singleton 255 `shouldParseTo` 255
-      B.singleton 123 `shouldParseTo` 123
-    testPutGet byte 1
-  describe "word16le" $ let ?action = word16le in do
-    it "parses pairs of bytes" $ do
-      B.pack [0xff, 0xff] `shouldParseTo` 0xffff
-    it "deals with endianity correctly" $ do
-      B.pack [0xe8, 0x03] `shouldParseTo` 0x03e8
-    testPutGet word16le 2
-  describe "word16be" $ let ?action = word16be in do
-    it "parses pairs of bytes" $ do
-      B.pack [0xff, 0xff] `shouldParseTo` 0xffff
-    it "deals with endianity correctly" $ do
-      B.pack [0x03, 0xe8] `shouldParseTo` 0x03e8
-    testPutGet word16be 2
-  describe "word32le" $ let ?action = word32le in do
-    testPutGet word32le 4
-    it "parses four bytes" $ do
-      B.pack [0xff, 0xff, 0xff, 0xff] `shouldParseTo` 0xffffffff
-    it "deals with endianity correctly" $ do
-      B.pack [0x03, 0xe8, 0x03, 0xe8] `shouldParseTo` 0xe803e803
-  describe "word32be" $ let ?action = word32be in do
-    testPutGet word32be 4
-    it "parses four bytes" $ do
-      B.pack [0xff, 0xff, 0xff, 0xff] `shouldParseTo` 0xffffffff
-    it "deals with endianity correctly" $ do
-      B.pack [0xe8, 0x03, 0xe8, 0x03] `shouldParseTo` 0xe803e803
-  describe "word64le" $ let ?action = word64le in do
-    it "parses eight bytes" $ do
-      B.pack [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
-        `shouldParseTo` 0xffffffffffffffff
-    it "deals with endianity correctly" $ do
-      B.pack [0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]
-        `shouldParseTo` 0xffeeddccbbaa9988
-    testPutGet word64le 8
-  describe "word64be" $ let ?action = word64be in do
-    it "parses eight bytes" $ do
-      B.pack [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
-        `shouldParseTo` 0xffffffffffffffff
-    it "deals with endianity correctly" $ do
-      B.pack [0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]
-        `shouldParseTo` 0x8899aabbccddeeff
-    testPutGet word64be 8
+  testByte
+  testWord16le
+  testWord16be
+  testWord32le
+  testWord32be
+  testWord64le
+  testWord64be
