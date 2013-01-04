ab8442031c6ea681b2cdffdd9296cf5007e4760d
startling <tdixon51793@gmail.com>
2013-01-04 17:30:55 -0500

    some more unit tests for endianity, courtesy of wikipedia.
    apparently I suck at endianity.

--- test.hs
+++ test.hs
@@ -1,4 +1,6 @@
 {-# Language Rank2Types #-}
+{-# Language ImplicitParams #-}
+{-# Language NoMonomorphismRestriction #-}
 module Main where
 -- base
 import Data.Word
@@ -26,12 +28,28 @@ testPutGet action size = do
     . property $ (B.pack <$> vectorOf size arbitrary) <&>
       \bs -> (serialize action <$> deserialize action bs) == Just bs
 
+-- | Check that something parses to the 
+shouldParseTo :: (?action :: Get b, Eq b, Show b, Default b)
+  => ByteString -> b -> Expectation
+a `shouldParseTo` b = deserialize ?action a `shouldBe` Just b
+
 main = hspec $ do
-  describe "byte" $ do
+  describe "byte" $ let ?action = byte in do
+    it "parses single bytes" $ do
+      B.singleton 255 `shouldParseTo` 255
+      B.singleton 123 `shouldParseTo` 123
     testPutGet byte 1
-  describe "word16le" $ do
+  describe "word16le" $ let ?action = word16le in do
+    it "parses pairs of bytes" $ do
+      B.pack [0xff, 0xff] `shouldParseTo` 0xffff
+    it "deals with endianity correctly" $ do
+      B.pack [0xe8, 0x03] `shouldParseTo` 0x03e8
     testPutGet word16le 2
-  describe "word16be" $ do
+  describe "word16be" $ let ?action = word16be in do
+    it "parses pairs of bytes" $ do
+      B.pack [0xff, 0xff] `shouldParseTo` 0xffff
+    it "deals with endianity correctly" $ do
+      B.pack [0x03, 0xe8] `shouldParseTo` 0x03e8
     testPutGet word16be 2
   describe "word32le" $ do
     testPutGet word32le 4
