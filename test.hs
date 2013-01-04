95cf606d348c73ed47b7015dca7d72d61b8a69d8
startling <tdixon51793@gmail.com>
2013-01-04 17:08:40 -0500

    got rid of the byteAt tests, since they're not really in scope anymore.

--- test.hs
+++ test.hs
@@ -27,26 +27,6 @@ testPutGet action size = do
       \bs -> (serialize action <$> deserialize action bs) == Just bs
 
 main = hspec $ do
-  describe "byteAt" $ do
-    it "gets the zeroeth byte of a Word8" . property $
-      \x -> (x ^. byteAt 0) == x
-    it "sets the zeroeth byte of a Word8" . property $
-      \x y -> (byteAt 0 .~ y $ (x :: Word8)) == y
-    it "gets the zeroeth byte of a Word16" $ do
-      (0xffff :: Word16) ^. byteAt 0 `shouldBe` 0xff
-      (0xff00 :: Word16) ^. byteAt 0 `shouldBe` 0xff
-      (0x00ff :: Word16) ^. byteAt 0 `shouldBe` 0x00
-    it "sets the zeroeth byte of a Word16" $ do
-      (byteAt 0 .~ 0xff $ 0x00ff :: Word16) `shouldBe` 0xffff
-      (byteAt 0 .~ 0x00 $ 0xffff :: Word16) `shouldBe` 0x00ff
-    it "gets the first byte of a Word16" $ do
-      (0xffff :: Word16) ^. byteAt 1 `shouldBe` 0xff
-      (0xff00 :: Word16) ^. byteAt 1 `shouldBe` 0x00
-      (0x00ff :: Word16) ^. byteAt 1 `shouldBe` 0xff
-    it "sets the first byte of a Word16" $ do
-      (byteAt 1 .~ 0x00 $ 0x00ff :: Word16) `shouldBe` 0x0000
-      (byteAt 1 .~ 0xff $ 0x0000 :: Word16) `shouldBe` 0x00ff
-      (byteAt 1 .~ 0xff $ 0xff00 :: Word16) `shouldBe` 0xffff
   describe "byte" $ do
     testPutGet byte 1
   describe "word16le" $ do
